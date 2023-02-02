21e0ea65e815f7c08290acdd74e7b8903dca9870
Gurdeep <khalsagudeep@gmail.com>
2023-02-02 18:02:35 +0530

    use styles and className in react

--- item-counter-app/src/component/counter.jsx
+++ item-counter-app/src/component/counter.jsx
@@ -4,11 +4,22 @@ class Counter extends Component {
   state = {
     count: 0,
   };
+
+  // for applying styles to more than one elements
+  //   styles = {
+  //     fontSize: 15,
+  //     fontWeight: "bold",
+  //   };
   render() {
     return (
       <React.Fragment>
-        <span>{this.formatCount()}</span>
-        <button>Increment</button>
+        <span
+          style={{ fontSize: 15, fontWeight: "bolder" }}
+          className="badge badge-primary m-2"
+        >
+          {this.formatCount()}
+        </span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
       </React.Fragment>
     );
   }
