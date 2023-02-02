57c51ff839f640efea2ed2e847ea6a1f64530b8e
Gurdeep <khalsagudeep@gmail.com>
2023-02-02 21:15:45 +0530

    dynamically changing classes

--- item-counter-app/src/component/counter.jsx
+++ item-counter-app/src/component/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
+    count: 1,
   };
 
   // for applying styles to more than one elements
@@ -15,13 +15,19 @@ class Counter extends Component {
       <React.Fragment>
         <span
           style={{ fontSize: 15, fontWeight: "bolder" }}
-          className="badge badge-primary m-2"
+          className={this.getBadgeClasses()}
         >
           {this.formatCount()}
         </span>
         <button className="btn btn-secondary btn-sm">Increment</button>
       </React.Fragment>
     );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
   }
 
   formatCount() {
