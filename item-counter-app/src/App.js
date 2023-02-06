47625be5e03a6d4b2748b9c40af6240651742ccc
Gurdeep <khalsagudeep@gmail.com>
2023-02-06 18:34:16 +0530

    feat:
    shifted a state up to form single sle source of truth

--- item-counter-app/src/App.js
+++ item-counter-app/src/App.js
@@ -1,17 +1,74 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 import Counters from "./component/Counters";
 import Navbar from "./component/navbar";
 
-function App() {
-  return (
-    <React.Fragment>
-      <Navbar />
-      <main className="container">
-        <Counters />
-      </main>
-    </React.Fragment>
-  );
+class App extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 0 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+    ],
+  };
+
+  handleDelete = (id) => {
+    this.setState({
+      counters: this.state.counters.filter((counter) => counter.id != id),
+    });
+  };
+
+  handleAdd = () => {
+    const { counters } = this.state;
+    counters.push({
+      id: counters.length ? counters[counters.length - 1].id + 1 : 1,
+      value: 0,
+    });
+    const newCounter = counters;
+    this.setState({
+      counters: newCounter,
+    });
+  };
+
+  decrementFunc = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index].value--;
+    this.setState({ counters });
+  };
+
+  incrementFunc = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index].value++;
+    this.setState({ counters });
+  };
+
+  handleReset = () => {
+    const counters = this.state.counters.map((counter) => {
+      counter.value = 0;
+      return counter;
+    });
+    this.setState({ counters });
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <Navbar />
+        <main className="container">
+          <Counters
+            onDelete={this.handleDelete}
+            onReset={this.handleReset}
+            onAdd={this.handleAdd}
+            onInc={this.incrementFunc}
+            onDec={this.decrementFunc}
+            Counters={this.state.counters}
+          />
+        </main>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
