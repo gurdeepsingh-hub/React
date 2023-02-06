1c549af4394d2cd288ad277dcb03e2e9201898a8
Gurdeep <khalsagudeep@gmail.com>
2023-02-06 19:16:00 +0530

    refactor
    convert navbar component to stateless functional component

--- item-counter-app/src/component/navbar.jsx
+++ item-counter-app/src/component/navbar.jsx
@@ -1,20 +1,18 @@
 import React, { Component } from "react";
 
-class Navbar extends Component {
-  render() {
-    return (
-      <nav className="navbar navbar-light bg-light">
-        <a className="navbar-brand">
-          Item Counter:{" "}
-          {this.props.onTotal() == 0
-            ? "Empty list"
-            : (this.props.onTotal() == 1
-                ? this.props.onTotal() + " item "
-                : this.props.onTotal() + " items ") + " in the list"}
-        </a>
-      </nav>
-    );
-  }
-}
+const Navbar = (props) => {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <a className="navbar-brand">
+        Item Counter:{" "}
+        {props.onTotal() == 0
+          ? "Empty list"
+          : (props.onTotal() == 1
+              ? props.onTotal() + " item "
+              : props.onTotal() + " items ") + " in the list"}
+      </a>
+    </nav>
+  );
+};
 
 export default Navbar;
