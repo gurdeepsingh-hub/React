4349797cb70a01e4d548f190979f92c0011aa04d
Gurdeep <khalsagudeep@gmail.com>
2023-02-07 12:53:18 +0530

    convert class to SFC

--- vidly/src/Components/Common/like.jsx
+++ vidly/src/Components/Common/like.jsx
@@ -1,19 +1,18 @@
 import React, { Component } from "react";
 
-class Like extends Component {
-  render() {
-    let classes = "fa fa-heart";
-    if (!this.props.liked) {
-      classes += "-o";
-    }
-    return (
-      <i
-        onClick={this.props.onClick}
-        className={classes}
-        aria-hidden="true"
-      ></i>
-    );
+function Like({ liked, onClick }) {
+  let classes = "fa fa-heart";
+  if (!liked) {
+    classes += "-o";
   }
+  return (
+    <i
+      onClick={onClick}
+      style={{ cursor: "pointer" }}
+      className={classes}
+      aria-hidden="true"
+    ></i>
+  );
 }
 
 export default Like;
