049e8a2f26ee547b716545bc612f25b0f0ea9811
Gurdeep <khalsagudeep@gmail.com>
2023-02-07 12:42:03 +0530

    feat: add option to like

--- vidly/src/Components/movieTable.jsx
+++ vidly/src/Components/movieTable.jsx
@@ -1,12 +1,25 @@
 import React, { Component } from "react";
 import { getMovies, deleteMovie } from "../Services/fakeMovieService.js";
 
+import Table from "./table.jsx";
+
 class MovieTable extends Component {
   constructor(props) {
     super(props);
   }
   state = {
     movies: getMovies(),
+  };
+
+  handleLike = (id) => {
+    const movies = this.state.movies;
+    const newMovie = movies.map((movie) => {
+      if (movie._id === id) {
+        movie.liked = !movie.liked;
+      }
+      return movie;
+    });
+    this.setState({ movies: newMovie });
   };
   render() {
     return (
@@ -16,53 +29,19 @@ class MovieTable extends Component {
             ? "There is no movie in database"
             : `Showing ${this.state.movies.length} movies in database`}
         </p>
-        {this.addTable()}
+        {
+          <Table
+            onDelete={(id) => this.handleDelete(id)}
+            onLike={(id) => this.handleLike(id)}
+            movies={this.state.movies}
+          />
+        }
       </React.Fragment>
     );
   }
 
   handleDelete(id) {
     this.setState(deleteMovie(id));
-  }
-
-  addTable() {
-    if (this.state.movies.length) {
-      return (
-        <table className="table">
-          <thead>
-            <tr>
-              <th>Title</th>
-              <th>Genre</th>
-              <th>Stock</th>
-              <th>Rate</th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.state.movies.map((movie) => {
-              return (
-                <tr key={movie._id}>
-                  <td key={"title" + movie._id}>{movie.title}</td>
-                  <td key={"genre" + movie._id}>{movie.genre.name}</td>
-                  <td key={"stock" + movie._id}>{movie.numberInStock}</td>
-                  <td key={"rate" + movie._id}>{movie.dailyRentalRate}</td>
-                  <td>
-                    <button
-                      className="btn btn-danger"
-                      onClick={() => {
-                        this.handleDelete(movie._id);
-                      }}
-                    >
-                      Delete
-                    </button>
-                  </td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      );
-    }
   }
 }
 
